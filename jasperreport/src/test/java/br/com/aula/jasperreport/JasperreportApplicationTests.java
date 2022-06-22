38021befbd97b676c2c0900b398eb771cf3cdd67
dvr.rafa@gmail.com <dvr.rafa@gmail.com>
2022-06-22 09:34:26 -0300

    first commit

---
+++ jasperreport/src/test/java/br/com/aula/jasperreport/JasperreportApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aula.jasperreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JasperreportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
